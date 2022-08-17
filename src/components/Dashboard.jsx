55d0a3e10a6140fcad9fc0f83456ad6eeaa9d6b9
Nazneen Oomatia <naz.chini@gmail.com>
2022-08-17 15:18:23 +0200

    Build layout to display visualisations

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import scrollreveal from "scrollreveal";
+
+export default function Dashboard() {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "bottom",
+      distance: "80px",
+      duration: 2000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        nav,
+    `,
+      {
+        opacity: 0,
+        interval: 100,
+      }
+    );
+  }, []);
+  return (
+    <Section>
+      <Navbar />
+      <div className="flex"></div>
+    </Section>
+  );
+}
+
+const Section = styled.section`
+  margin-left: 18vw;
+  padding: 2rem;
+  height: 100%;
+  .flex {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 1rem;
+    margin-top: 2rem;
+  }
+
+  @media screen and (min-width: 280px) and (max-width: 900px) {
+    margin-left: 0;
+    }
+  }
+`;
