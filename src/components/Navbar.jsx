7a3b03fa27e3d437e0ddcdaf3c614048e1359f77
Nazneen Oomatia <naz.chini@gmail.com>
2022-08-17 15:17:43 +0200

    Create basic navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Navbar() {
+  return (
+    <Nav>
+      <div className="title">
+        <h4>Hi Waldemar,</h4>
+        <h1>
+          Welcome to <span>YOUR DASHBOARD</span>
+        </h1>
+      </div>
+    </Nav>
+  );
+}
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  .title {
+    h1 {
+      span {
+        color: #4c8bf5;
+      }
+    }
+  }
+
+  @media screen and (min-width: 280px) and (max-width: 900px) {
+    flex-direction: column;
+    .title {
+      h1 {
+        span {
+          display: block;
+
+          margin: 1rem 0;
+          /* letter-spacing: 0; */
+        }
+      }
+    }
+  }
+`;
