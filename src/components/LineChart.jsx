f669c766320efc0959fb20cbd524d88d193b4665
Nazneen Oomatia <naz.chini@gmail.com>
2022-08-18 14:49:26 +0200

    Integrate styling changes internally

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import styled from "styled-components";
-import { cardStyles } from "./ReusableStyles";
 import {
   LineChart,
   ResponsiveContainer,
@@ -18,35 +16,29 @@ const ageSorted = data.sort((a, b) => a.Age - b.Age);
 export default function Linechart() {
   return (
     <Section>
-      <div className="chart">
-        <ResponsiveContainer width="90%" aspect={1}>
-          <LineChart
-            data={ageSorted}
-            margin={{ top: 20, left: 20, right: 20, bottom: 50 }}
-          >
-            {/* <CartesianGrid /> */}
-            <XAxis dataKey="Age" interval={"preserveStartEnd"} />
-            <YAxis></YAxis>
-            <Legend />
-            <Tooltip />
-            <Line dataKey="Fare" stroke="black" activeDot={{ r: 8 }} />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+      <h1>Age vs. Fare Chart</h1>
+      <ResponsiveContainer width="90%" aspect={1}>
+        <LineChart
+          data={ageSorted}
+          margin={{ top: 20, left: 20, right: 20, bottom: 50 }}
+        >
+          {/* <CartesianGrid /> */}
+          <XAxis dataKey="Age" interval={"preserveStartEnd"} />
+          <YAxis></YAxis>
+          <Legend />
+          <Tooltip />
+          <Line dataKey="Fare" stroke="black" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
     </Section>
   );
 }
 
 const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  min-height: 20rem;
-  ${cardStyles}
+  text-align: center;
+  border-radius: 1rem;
+  -webkit-box-shadow: 0px 0px 10px 10px rgba(208, 208, 208, 0.5);
+  box-shadow: 0px 0px 10px 10px rgba(208, 208, 208, 0.5);
   padding: 2rem 0 0 0;
-
-  .chart {
-    height: 70%;
-    }
   }
 `;
