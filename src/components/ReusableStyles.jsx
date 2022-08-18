1e1adc76e0961f8dcb986cb74db30ab8dd149c03
Nazneen Oomatia <naz.chini@gmail.com>
2022-08-19 00:28:29 +0200

    Integrate common styles into global styles

--- src/components/ReusableStyles.jsx
+++
@@ -1,8 +0,0 @@
-import { css } from "styled-components";
-
-export const cardStyles = css`
-  padding: 1rem 2rem 3rem 2rem;
-  border-radius: 1rem;
-  -webkit-box-shadow: 0px 0px 10px 10px rgba(208, 208, 208, 0.5);
-  box-shadow: 0px 0px 10px 10px rgba(208, 208, 208, 0.5);
-`;
